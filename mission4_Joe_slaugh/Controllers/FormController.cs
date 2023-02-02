8ba94f2283db08ea7bb196fe3398f6975d535e58
JSlaugh <J@slaugh.net>
2023-02-01 22:18:55 -0700

    everything up untill the model

---
+++ mission4_Joe_slaugh/Controllers/FormController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mission4_Joe_slaugh.Controllers
+{
+    public class FormController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
