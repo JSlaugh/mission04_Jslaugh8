025aa0d4786ff013456c3fa773e2c5edbc4310f0
JSlaugh <93290565+JSlaugh@users.noreply.github.com>
2023-02-01 21:30:17 -0700

    test

---
+++ mission4_Joe_slaugh/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mission4_Joe_slaugh.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+
+            return View();
+        }
+
+    }
+}
--- mission4_Joe_slaugh/Startup.cs
+++ mission4_Joe_slaugh/Startup.cs
@@ -16,6 +16,7 @@ namespace mission4_Joe_slaugh
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,9 +27,16 @@ namespace mission4_Joe_slaugh
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-            app.UseDefaultFiles();
             app.UseStaticFiles();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                    );
+            }
+               );
         }
     }
 }
